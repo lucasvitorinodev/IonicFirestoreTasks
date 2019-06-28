4c751bb2dcc47177a2d630d5f1bdbede31ee9c0e
lucas <lucas.henrique@tuntscorp.com>
2019-06-28 01:01:10 -0300

    feat(login): form with validators for email and password

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {  ReactiveFormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 
 @NgModule({
   exports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
   ]
 })
