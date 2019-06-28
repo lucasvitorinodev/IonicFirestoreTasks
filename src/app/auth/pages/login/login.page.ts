37aab5bd5a0bf083ddbc3cd918b72dd54c43cb14
lucas <lucas.henrique@tuntscorp.com>
2019-06-28 01:59:35 -0300

    feat(login): sign up action

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FindValueOperator} from 'rxjs/internal/operators/find';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,12 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 export class LoginPage implements OnInit {
 
   authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Create account'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
   constructor(private fb: FormBuilder) {
   }
@@ -30,6 +37,20 @@ export class LoginPage implements OnInit {
 
   get password(): FormControl {
     return this.authForm.get('password') as FormControl;
+  }
+
+  get name(): FormControl {
+    return this.authForm.get('name') as FormControl;
+  }
+
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sign Up';
+    this.configs.actionChange = isSignIn ? 'Create Account' : 'Already have an account';
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
   }
 
   onSubmit(): void {
