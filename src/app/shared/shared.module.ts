64b2c9005a9487d30c38846fb2300424c717954c
lucas <lucas.henrique@tuntscorp.com>
2019-06-27 21:28:14 -0300

    feat(app): all
    - Initial commit
    - Add login page
    - Add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {IonicModule} from '@ionic/angular';
+
+@NgModule({
+  exports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ]
+})
+export class  SharedModule { }
