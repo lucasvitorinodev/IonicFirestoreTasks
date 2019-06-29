f9d3c527b616538e5f23550ea56f26224c741fd1
lucas <lucas.henrique@tuntscorp.com>
2019-06-29 02:26:18 -0300

    feat(auth.service): add signInWithEmail method

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {auth} from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+  private signInWithEmail({email, password}): Promise<auth.UserCredential> {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+  }
 }
