413700f12f30256b09fedebc426867741f70c7cd
lucas <lucas.henrique@tuntscorp.com>
2019-07-10 22:06:54 -0300

    feat(firestore.class): create a method to get all registers from a collection on firebase

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,2 +1,18 @@
+import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 export abstract class FirestoreClass<T> {
+  protected collection: AngularFirestoreCollection<T>;
+
+  constructor(protected db: AngularFirestore) {
+  }
+
+  protected setCollection(path: string, queryFn?: QueryFn): void {
+    this.collection = path ? this.db.collection(path, queryFn) : null;
+  }
+
+  getAll(): Observable<T[]> {
+    return this.collection.valueChanges();
+  }
+
 }
