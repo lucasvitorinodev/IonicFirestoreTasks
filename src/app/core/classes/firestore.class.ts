6e1e98ce27b96db2e57e6e89fe8cd3cb694bf49e
lucas <lucas.henrique@tuntscorp.com>
2019-07-10 22:32:04 -0300

    feat(firestore.class): create a method to update or create documents on firestore

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,7 +1,7 @@
 import { AngularFirestore, AngularFirestoreCollection, QueryFn } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export abstract class FirestoreClass<T> {
+export abstract class FirestoreClass<T extends { id: string }> {
   protected collection: AngularFirestoreCollection<T>;
 
   constructor(protected db: AngularFirestore) {
@@ -11,8 +11,19 @@ export abstract class FirestoreClass<T> {
     this.collection = path ? this.db.collection(path, queryFn) : null;
   }
 
+  private setItem(item: T, operation: string): Promise<T> {
+    return this.collection
+      .doc<T>(item.id)
+      [operation](item)
+      .then(() => item);
+  }
+
   getAll(): Observable<T[]> {
     return this.collection.valueChanges();
   }
 
+  create(item: T): Promise<T> {
+    item.id = this.db.createId();
+    return this.setItem(item, 'set');
+  }
 }
