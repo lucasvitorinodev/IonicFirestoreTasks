92acd32517a32f3ced64a7d70035f53939ce9d99
lucas <lucas.henrique@tuntscorp.com>
2019-07-15 19:03:55 -0300

    feat(tasks.service): set collection for logged user

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -3,13 +3,25 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 import { Firestore } from 'src/app/core/classes/firestore.class';
 import { Task } from '../models/task.model';
+import { AuthService } from '../../core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService extends Firestore<Task> {
 
-  constructor(db: AngularFirestore) {
+  constructor(private authService: AuthService, db: AngularFirestore) {
     super(db);
+    this.init();
+  }
+
+  private init(): void {
+    this.authService.authState$.subscribe(user => {
+      if (user) {
+        this.setCollection(`/users/${user.uid}/tasks`);
+        return;
+      }
+      this.setCollection(null);
+    });
   }
 }
