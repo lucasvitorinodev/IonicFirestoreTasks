849f4b8b89348188dd670a2ae6dd1b0141801e2c
lucas <lucas.henrique@tuntscorp.com>
2019-07-16 00:23:39 -0300

    feat(task-routing): add create page to tasks routing module

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -8,16 +8,20 @@ const routes: Routes = [
     canActivateChild: [AuthGuard],
     children: [
       {
+        path: 'create',
+        loadChildren: './pages/task-save/task-save.module#TaskSavePageModule'
+      },
+      {
         path: '',
         loadChildren: './pages/tasks-list/tasks-list.module#TasksListPageModule'
       }
     ]
   },
-  { path: 'task-save', loadChildren: './pages/task-save/task-save.module#TaskSavePageModule' }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TasksRoutingModule { }
+export class TasksRoutingModule {
+}
