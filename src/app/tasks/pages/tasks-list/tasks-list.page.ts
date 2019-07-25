63176429ef31ae181692e8ac8b7273e489418ada
lucas <lucas.henrique@tuntscorp.com>
2019-07-25 01:06:39 -0300

    feat(tasks-list): add loading on ngonint

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -4,6 +4,7 @@ import { Task } from '../../models/task.model';
 import { TasksService } from '../../services/tasks.service';
 import { NavController } from '@ionic/angular';
 import { OverlayService } from '../../../core/services/overlay.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tasks-list',
@@ -19,8 +20,10 @@ export class TasksListPage implements OnInit {
               private overlayService: OverlayService) {
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    const loading = await this.overlayService.loading();
     this.tasks$ = this.tasksService.getAll();
+    this.tasks$.pipe(take(1)).subscribe(tasks => loading.dismiss());
   }
 
   onUpdate(task: Task): void {
