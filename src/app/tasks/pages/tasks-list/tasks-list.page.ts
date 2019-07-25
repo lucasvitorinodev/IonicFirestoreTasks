ccef6dee30c9edd0d4bfcdf5c226c96ef88efa13
lucas <lucas.henrique@tuntscorp.com>
2019-07-25 00:09:51 -0300

    feat(tasks-routing): navigate to edit route

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from '../../models/task.model';
 import { TasksService } from '../../services/tasks.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tasks-list',
@@ -12,11 +13,16 @@ export class TasksListPage implements OnInit {
 
   tasks$: Observable<Task[]>;
 
-  constructor(private tasksService: TasksService) {
+  constructor(private tasksService: TasksService,
+              private navCtrl: NavController) {
   }
 
   ngOnInit(): void {
     this.tasks$ = this.tasksService.getAll();
+  }
+
+  onUpdate(task: Task): void {
+    this.navCtrl.navigateForward(`/tasks/edit/${task.id}`);
   }
 
 }
