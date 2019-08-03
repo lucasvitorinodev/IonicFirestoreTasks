2a72ac0f2442ef0ff62151c96fa4b2e39ae46ca3
lucas <lucas.henrique@tuntscorp.com>
2019-08-03 15:33:37 -0300

    feat(app): add logout button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
+import { LogoutButtonComponent } from './components/logout-button/logout-button.component';
 
 @NgModule({
-  declarations: [MenuToggleComponent],
+  declarations: [MenuToggleComponent, LogoutButtonComponent],
   imports: [IonicModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     IonicModule,
-    MenuToggleComponent
+    MenuToggleComponent,
+    LogoutButtonComponent
   ]
 })
 export class SharedModule {
