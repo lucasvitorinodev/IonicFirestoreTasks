180779e6e2f5731dcf2ad2b086eb893b0da4839b
lucas <lucas.henrique@tuntscorp.com>
2019-08-03 16:47:33 -0300

    feat(env): add prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB4YAgjer0nnHPIb5R1pquUGzGe6VjznjY',
+    authDomain: 'ionic-firestore-tasks-e0011.firebaseapp.com',
+    databaseURL: 'https://ionic-firestore-tasks-e0011.firebaseio.com',
+    projectId: 'ionic-firestore-tasks-e0011',
+    storageBucket: '',
+    messagingSenderId: '132649755321',
+    appId: '1:132649755321:web:c5e742b218f34561'
+  }
 };
