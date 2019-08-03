1ad3f8b378e707d8462dedee35ee1ef1e4b21120
lucas <lucas.henrique@tuntscorp.com>
2019-08-03 17:45:06 -0300

    feat(core): firestore off

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -16,7 +16,9 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     IonicModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule.enablePersistence({
+      experimentalTabSynchronization: true
+    })
   ],
   exports: [
     BrowserModule,
