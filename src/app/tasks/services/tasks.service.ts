2be3e57e99c96e64dd233cdf9e91ebcac707027b
lucas <lucas.henrique@tuntscorp.com>
2019-08-03 00:57:30 -0300

    feat(tasks-list): order task list by done and name

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -18,7 +18,9 @@ export class TasksService extends Firestore<Task> {
   private init(): void {
     this.authService.authState$.subscribe(user => {
       if (user) {
-        this.setCollection(`/users/${user.uid}/tasks`);
+        this.setCollection(`/users/${user.uid}/tasks`, (ref) => {
+          return ref.orderBy('done', 'asc').orderBy('title', 'asc');
+        });
         return;
       }
       this.setCollection(null);
