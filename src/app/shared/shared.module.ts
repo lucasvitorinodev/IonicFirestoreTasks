6d61e7f1f199f8e80904209789f8b33bb0059f50
lucas <lucas.henrique@tuntscorp.com>
2019-08-03 15:09:39 -0300

    feat(app): add menu component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  ReactiveFormsModule} from '@angular/forms';
-import {IonicModule} from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { MenuToggleComponent } from './components/menu-toggle/menu-toggle.component';
 
 @NgModule({
+  declarations: [MenuToggleComponent],
+  imports: [IonicModule],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     IonicModule,
+    MenuToggleComponent
   ]
 })
-export class  SharedModule { }
+export class SharedModule {
+}
